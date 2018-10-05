cfd01f3d5625d8076e6138c837e5f7977e754c7b
p-funky <adeyinka.alabi@andela.com>
2018-10-05 11:04:17 +0100

    feat: add functionality for making ajax requests

--- public/app.js
+++ public/app.js
@@ -1,3 +1,63 @@
 const app = {};
 
-console.log("Hello console world!!!");
+app.config = {
+    sessionToken: false
+};
+
+app.client = {};
+
+app.client.request = (headers, path, method, queryStringObject, payload, callback) => {
+    headers = typeof headers === 'object' && headers !== null ? headers : {};
+    path = typeof path === 'string' ? path : '/';
+    method = typeof method === 'string' && ['POST', 'GET', 'PUT', 'DELETE'].indexOf(method.toUpperCase()) > -1 ?
+        method.toUpperCase() : 'GET';
+    queryStringObject = typeof queryStringObject === 'object' && queryStringObject !== null ? queryStringObject : {};
+    payload = typeof payload === 'object' && payload !== null ? payload : {};
+    callback = typeof callback === 'function' ? callback : false;
+
+    const requestUrl = path + '?';
+    let counter = 0;
+    for (let queryKey in queryStringObject) {
+        if (queryStringObject.hasOwnProperty(queryKey)) {
+            counter++;
+            if (counter > 1) {
+                requestUrl += '&';
+            }
+            requestUrl += queryKey + '=' + queryStringObject[queryKey];
+        }
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, requestUrl, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    for (let headerKey in headers) {
+        if (headers.hasOwnProperty(headerKey)) {
+            xhr.setRequestHeader(headerkey, headers[headerKey]);
+        }
+    }
+
+    if (app.config.sessionToken) {
+        xhr.setRequestHeader('token', app.config.sessionToken.id);
+    }
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            const statusCode = xhr.status;
+            const responseReturned = xhr.responseText;
+
+            if (callback) {
+                try {
+                    const parsedResponse = JSON.parse(responseReturned);
+                    callback(statusCode, parsedResponse);
+                } catch (error) {
+                    callback(statusCode, false);
+                }
+            }
+        }
+
+    }
+
+    const payloadString = JSON.stringify(payload);
+    xhr.send(payloadString);
+};
